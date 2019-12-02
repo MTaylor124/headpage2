fdb7e85810cc345618264cd01b78cd12e6a23a8f
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-12-02 10:55:58 -0500

    Initial commit/file management

---
+++ src/templates/class.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class MyClass extends Component {
+    constructor(props) {
+        super (props)
+
+        this.state = {
+
+        }
+    }
+    render () {
+        return (
+            <div> default class</div>
+        )
+    }
+}
---
+++ src/templates/functionalcomponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyFunction () {
+    return (
+        <div>standard function</div>
+    )
+}
